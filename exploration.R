aacc034a5c7cd32d5f4bfebc9dc5cbae95f6e2d7
maheeeb <indramahibzuhair@apps.ipb.ac.id>
2024-09-02 08:27:49 +0700

    update pertemuan 2

--- exploration.R
+++ exploration.R
@@ -12,6 +12,9 @@ dt$tanggal <- as.Date(dt$tanggal,format="%Y-%m-%d")
 dt.ts <- ts(dt$penumpang.berangkat)
 plot(dt.ts)
 
+
+str(dt)
+
 # ACF
 acf(dt.ts, lag.max=30)
 
@@ -23,6 +26,12 @@ adf.test(dt.ts)
 library(dplyr)
 dt <- read.csv("D:\\IPB\\5\\MPDW\\data\\data_0101-3107_mpdw _csv.csv")
 str(dt)
+
+names(dt) <- tolower(names(dt))
+
+dt$tanggal <- as.Date(dt$tanggal,format="%Y-%m-%d")
+
+
 
 dt_cgk <- dt %>% filter(ID.Prasarana == "CGK")
 str(dt_cgk)
@@ -37,21 +46,65 @@ adf.test(dt_cgk.ts)
 
 airport_kal <- c("BPN","BDJ","PKN","PKY","PNK", "AAP","TRK","BEJ")
 
-dt_kal <- dt %>% filter(ID.Prasarana %in% airport_kal)
-dt_kal <- dt_kal %>% select(Tanggal, Penumpang.Berangkat, ID.Prasarana)
+dt_kal <- dt %>% filter(id.prasarana %in% airport_kal)
+dt_kal <- dt_kal %>% select(tanggal, penumpang.berangkat, ID.Prasarana)
 unique(dt_kal$ID.Prasarana) %>% length()
 
-dt_kal_sum <- dt_kal %>% group_by(Tanggal) %>% 
-  summarise(Penumpang.Berangkat = sum(Penumpang.Berangkat))
-dim(dt_kal_sum)
-summary(dt_kal_sum)
+str(dt_kal)
 
-dt_kal_sum$Tanggal <- as.Date(dt_kal_sum$Tanggal,format="%Y-%m-%d")
-summary(dt_kal_sum)
-dt_kal_sum[duplicated(dt_kal_sum$Tanggal),]
-
-kalsum.ts <- ts(dt_kal_sum$Penumpang.Berangkat)
-plot(kalsum.ts)  
-adf.test(kalsum.ts)
+ggplot(dt_kal,aes(x=tanggal, y=penumpang.berangkat,col=as.factor(id.prasarana)))+
+  geom_line()
 
 
+dt_BPN <- dt_kal %>% filter(id.prasarana == "BPN")
+
+ggplot(dt_BPN,aes(x=tanggal))+
+  geom_line(aes(y=penumpang.berangkat),col="blue")+
+  geom_line(aes(y=penumpang.tiba),col="red")
+
+
+
+
+tom <- read.csv("D:\\IPB\\5\\MPDW\\data\\tomtom_20_27.csv")
+str(tom)
+tom$Time1 <- as.Date(tom$Time)
+str(tom)
+
+datetime <- "2024-08-28 14:35:00"
+tom$Time1 <- as.POSIXct(tom$Time, format="%Y-%m-%d %H:%M:%S")
+str(tom)
+
+ggplot(tom, aes(x=Time1, y=Travel.Time)) + geom_line()
+
+tom2 <- read.csv("D:\\IPB\\5\\MPDW\\data\\traffic_data-03-14-8-to-03-21-8.csv")
+
+View(tom2)
+str(tom2)
+tom2$Time1 <- as.POSIXct(tom2$Time, format="%Y-%m-%d %H:%M:%S")
+str(tom2)
+
+ggplot(tom2, aes(x=Time1, y=Jams.Length)) + geom_line()
+
+View(tom)
+tomawal <- tom2 %>% filter(Time1 < "2024-08-28 13:00:00")
+View(tomawal)
+
+tom2[1:154,] %>% View()
+
+gabung <- rbind(tom2[1:154,], tom)
+dim(gabung)
+
+ggplot(gabung) + 
+  geom_line(aes(x=Time1, y=Jams.Length, color="jamsLen")) +
+  geom_line(aes(x=Time1, y=Travel.Time, color="travelTime")) +
+  geom_line(aes(x=Time1, y=Jams.Count, color="jamsCount"))+
+  geom_line(aes(x=Time1, y=Live.Traffic, color="index")) +
+  scale_color_manual(values=c("jamsLen"="red",
+                              "travelTime"="blue",
+                              "jamsCount"="green",
+                              "index"="black"))
+
+write.csv(gabung, "D:\\IPB\\5\\MPDW\\data\\gabung.csv")
+
+
+  
